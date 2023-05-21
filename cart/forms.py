23fc6f0ee7f73459fba178b7138af0fa905c970d
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-21 14:40:43 +0330

    translating python code ,use makemessages and compilemessages ,ngettext for plural

--- cart/forms.py
+++ cart/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class ProductCartForm(forms.Form):
 	PRODUCT_QUANTITY_CHOICES=[(i,str(i)) for i in range(1,11)]
-	quantity=forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int)
+	quantity=forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int,label=_('quantity'))
 	override=forms.BooleanField(widget=forms.HiddenInput,required=False,initial=False)
--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,8 +150,8 @@ USE_TZ = True
 
 
 LANGUAGES=[
-            ('en','english'),
-            ('fa','Persian')
+            ('en',_('english')),
+            ('fa',_('Persian'))
 ]
 
 LOCALE_PATHS=[
