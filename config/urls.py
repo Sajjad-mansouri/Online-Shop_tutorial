a824320f44a004c1190a98a705827dc3f1b0b1ac
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-21 16:43:56 +0330

    translating url patterns

--- config/urls.py
+++ config/urls.py
@@ -19,15 +19,19 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('cart/',include('cart.urls')),
-    path('order/',include('orders.urls')),
-    path('coupon/',include('coupons.urls')),
+    path(_('cart/'),include('cart.urls')),
+    path(_('order/'),include('orders.urls')),
+    path(_('coupon/'),include('coupons.urls')),
     path('rosetta/', include('rosetta.urls')),
     path('',include('shop.urls'))
 )
+# urlpatterns += [
+# path()
+# ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
